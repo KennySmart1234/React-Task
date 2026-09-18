2d630c21128213123e87d1e1afe5ce2cb37f32cd
KennySmart1234 <stephenkehindeolatunji@gmail.com>
2026-09-18 16:50:27 +0100

    sign-in and sign-out

--- myecommerce/src/api/dummyDataApi.js
+++ myecommerce/src/api/dummyDataApi.js
@@ -1,8 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
+const URL = import.meta.env.VITE_APP_BASEURL
+
 export const dummyDataApi = createApi({
+
     reducerPath: "dummyApi",
-    baseQuery: fetchBaseQuery({baseUrl: "https://dummyjson.com/"}),
+    baseQuery: fetchBaseQuery({baseUrl: URL}),
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (body) => ({
